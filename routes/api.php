e3c5021c61ebfd3a57797221546b959bdbe128c9
TUF GAMING F15 <iftikharazharchaudhry@gmail.com>
2025-07-29 09:53:37 +0700

    create new api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
@@ -11,4 +12,12 @@ Route::prefix('books')->group(function () {
     Route::post('/', [BookController::class, 'store']);         // POST /api/books - Tambah buku baru
     Route::put('/{id}', [BookController::class, 'update']);     // PUT /api/books/1 - Perbarui buku
     Route::delete('/{id}', [BookController::class, 'destroy']); // DELETE /api/books/1 - Hapus buku
+});
+
+Route::prefix('product')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);          // GET /books
+    Route::get('/{id}', [ProductController::class, 'show']);       // GET /books/{id}
+    Route::post('/', [ProductController::class, 'store']);         // POST /books
+    Route::put('/{id}', [ProductController::class, 'update']);     // PUT /books/{id}
+    Route::delete('/{id}', [ProductController::class, 'destroy']); // DELETE /books/{id}
 });
